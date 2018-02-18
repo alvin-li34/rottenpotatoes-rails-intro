18e816fd1a82136aa398da8ed336837f45e600d0
EC2 Default User <ec2-user@ip-172-31-36-117.us-east-2.compute.internal>
2018-02-18 18:49:05 +0000

    rating change

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.ratings
+    @all_ratings = Movie.rating
     
     if params[:title] == "sort"
       @movies = Movie.all.order(:title)
