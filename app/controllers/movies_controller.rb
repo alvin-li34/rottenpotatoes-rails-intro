12dedb25c7369650f28d7883d9344e654102c7fa
EC2 Default User <ec2-user@ip-172-31-36-117.us-east-2.compute.internal>
2018-02-18 22:36:48 +0000

    test sort session 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,7 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = ['G', 'PG', 'PG-13', 'R']
     #@sort = params[:sort] || session[:sort]
-    if params[:sort] || session[:sort]
+    if params[:sort]
       session[:sort] = params[:sort]
       @movies = Movie.all.order(params[:sort])
     elsif session[:sort]
