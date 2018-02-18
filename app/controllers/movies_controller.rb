f00dec4f51d4be247f055e10627322b1c4d5202a
EC2 Default User <ec2-user@ip-172-31-36-117.us-east-2.compute.internal>
2018-02-18 19:14:24 +0000

    checkbox test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = ['G', 'PG', 'PG-13']
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
     
     
     if params[:title] == "sort"
