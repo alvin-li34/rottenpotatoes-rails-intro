bdb789f2071d1d1c9d5d3fa76713d96ef42e6130
EC2 Default User <ec2-user@ip-172-31-36-117.us-east-2.compute.internal>
2018-02-18 21:21:52 +0000

    check sorted titles

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,12 +13,10 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = ['G', 'PG', 'PG-13', 'R']
     
-    if params[:title] == "sort"
-      session[:title] = params[:title]
-      @movies = Movie.all.order(:title)
-    elsif params[:release_date] == "sort"
-      session[:release_date] = params[:release_date]
-      @movies = Movie.all.order(:release_date)
+    if params[:sort]
+      
+      @movies = Movie.all.order(params[:sort])
+    
     else
       @movies = Movie.all
     end
